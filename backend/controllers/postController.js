400ca970c207dd8476a7d7484c4e17b50bf6ae27
Navleen kaur <kaurnavleen0621@gmail.com>
2024-11-21 20:52:15 +0530

    Create Post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,8 +1,47 @@
+import  Post from "../models/postModel.js";
+//import { response } from "express";
 import { Response } from "../utils/response.js"
+import User from "../models/userModel.js";
+import cloudinary from "cloudinary";
+import { message } from "../utils/message.js";
 
 export const createPost = async (req, res) => {
     try {
-        
+        //Parsing body data
+        const{image,caption,location}=req.body;
+        //checking body data
+        if(!caption){
+            return Response(res,400,false,message.missingFieldsMessage);
+        }
+        //check image
+        if(!image){
+            return Response(res,400,false,message.imageMissingMessage);
+        }
+        //upload image to cloudinary
+        let imageUpload=await cloudinary.v2.uploader.upload(image,{
+            folder:'posts'
+        })
+        //create post
+        let post=await Post.create({
+            image:{
+                public_id:imageUpload.public_id,
+                url:imageUpload.url
+        },
+        caption,
+        location
+        })
+
+        //Set owner details
+        post.owner=req.user._id;
+        await post.save();
+
+        //Set post in user
+        let user=await User.findById(req.user._id);
+        user.posts.unshift(post._id);
+        await user.save();
+
+        //Send response
+        Response(res,201,true,message.postCreateMessage,post);
     } catch (error) {
         Response(res, 500, false, error.message);
     }
